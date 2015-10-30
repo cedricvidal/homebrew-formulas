af2830150567df0055d187f083153945d8a66856
Cédric Vidal <cedric@vidal.biz>
2015-10-30 01:18:10 +0100

    jgitflow-semver

---
+++ jgitflow-semver.rb
@@ -0,0 +1,12 @@
+class JgitflowSemver < Formula
+  desc "JGit Flow Semver"
+  homepage "https://github.com/cedricvidal/jgitflow-semver"
+  url "https://bintray.com/artifact/download/cedric-vidal/jgitflow-semver/com/quicksign/jgitflow-semver/jgitflow-semver/0.2.1/jgitflow-semver-0.2.1-script.sh", :using => :nounzip
+  version "0.2.1"
+  sha256 "ef88f4c6a81c78b81fd9b277f2fccd947f8c84fdafaa2ae22d507aa533afcd04"
+
+  def install
+    bin.install "jgitflow-semver-0.2.1-script.sh" => "jgitflow-semver"
+  end
+
+end
