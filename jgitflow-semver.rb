69d90e6f7a1885f3eb26d6f6aabdb1660a85142f
Cédric Vidal <cedric@vidal.biz>
2016-01-11 19:20:37 +0100

    Fixed sha2

--- jgitflow-semver.rb
+++ jgitflow-semver.rb
@@ -3,7 +3,7 @@ class JgitflowSemver < Formula
   homepage "https://github.com/cedricvidal/jgitflow-semver"
   url "https://bintray.com/artifact/download/cedric-vidal/jgitflow-semver/com/quicksign/jgitflow-semver/jgitflow-semver/0.2.4/jgitflow-semver-0.2.4-script.sh", :using => :nounzip
   version "0.2.4"
-  sha256 "36c0c30abbe62528c106957077fd146345784bff"
+  sha256 "2656f64c3d17ffdd20f467e3e91ea503e283e79887cabbeefc2983525085f179"
 
   def install
     bin.install "jgitflow-semver-0.2.4-script.sh" => "jgitflow-semver"
