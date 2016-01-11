703ecf4b02cd2698c5030efba86ca287e7c732b4
Cédric Vidal <cedric@vidal.biz>
2016-01-11 19:17:21 +0100

    Upgraded jgitflow-semver to 0.2.4

--- jgitflow-semver.rb
+++ jgitflow-semver.rb
@@ -1,12 +1,12 @@
 class JgitflowSemver < Formula
   desc "JGit Flow Semver"
   homepage "https://github.com/cedricvidal/jgitflow-semver"
-  url "https://bintray.com/artifact/download/cedric-vidal/jgitflow-semver/com/quicksign/jgitflow-semver/jgitflow-semver/0.2.3/jgitflow-semver-0.2.3-script.sh", :using => :nounzip
-  version "0.2.3"
-  sha256 "853ddce256146b64ea642024c0585aa2a316255dc4f090e3a0abed8c2824eebf"
+  url "https://bintray.com/artifact/download/cedric-vidal/jgitflow-semver/com/quicksign/jgitflow-semver/jgitflow-semver/0.2.4/jgitflow-semver-0.2.4-script.sh", :using => :nounzip
+  version "0.2.4"
+  sha256 "36c0c30abbe62528c106957077fd146345784bff"
 
   def install
-    bin.install "jgitflow-semver-0.2.3-script.sh" => "jgitflow-semver"
+    bin.install "jgitflow-semver-0.2.4-script.sh" => "jgitflow-semver"
   end
 
 end
